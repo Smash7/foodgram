3fc003048f1d0c89fcd2c4c42aeea18f9897dae1
Eugene Martynov <martynov005@mail.ru>
2024-07-30 15:47:03 +0200

    fix pep8

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,16 +1,21 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import AvatarUploadView, IngredientViewSet, RecipeViewSet, SubscriptionViewSet, TagViewSet
+from .views import (AvatarUploadView, IngredientViewSet, RecipeViewSet,
+                    SubscriptionViewSet, TagViewSet)
 router = DefaultRouter()
 router.register(r'tags', TagViewSet, basename='tag-detail')
 router.register(r'recipes', RecipeViewSet, basename='recipe-detail')
-router.register(r'ingredients', IngredientViewSet, basename='ingredient-detail')
-router.register(r'users/subscriptions', SubscriptionViewSet, basename='subscription')
+router.register(r'ingredients', IngredientViewSet,
+                basename='ingredient-detail')
+router.register(r'users/subscriptions', SubscriptionViewSet,
+                basename='subscription')
 
 djoser_urls = [
     path('', include('djoser.urls')),
-    path('users/<int:id>/subscribe/', SubscriptionViewSet.as_view({'post': 'subscribe'}), name='user-subscribe'),
+    path('users/<int:id>/subscribe/',
+         SubscriptionViewSet.as_view({'post': 'subscribe'}),
+         name='user-subscribe'),
     path('users/<int:id>/unsubscribe/',
          SubscriptionViewSet.as_view({'delete': 'unsubscribe'}),
          name='user-unsubscribe'),
