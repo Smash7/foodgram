faf5a7c2ff1a443235dea43e4c41f94bf639b3a4
Eugene Martynov <martynov005@mail.ru>
2024-07-31 19:36:59 +0200

    create import_ingredients.py.py management command

---
+++ backend/recipes/management/commands/import_ingredients.py
@@ -0,0 +1,15 @@
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Import ingredients from JSON file'
+    manage_file = os.path.join(settings.BASE_DIR, 'manage.py')
+
+    def add_arguments(self, parser):
+        parser.add_argument('file', type=str, help='Path to JSON file')
+
+    def handle(self, file, *args, **kwargs):
+        os.system(f'python3 {self.manage_file} loaddata {file}')
