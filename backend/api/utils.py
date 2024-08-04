56df4b63140a3455039e07fc2942e1c915b23eb8
Eugene Martynov <martynov005@mail.ru>
2024-08-04 16:16:33 +0200

    refactor utils.py

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -7,14 +7,14 @@ def generate_shopping_list_text(ingredient_quantities, recipes):
     product_header = 'Продукты:'
     products = '\n'.join(
         [
-            f'{index + 1}. {item["ingredient__name"].capitalize()} --'
+            f'{index}. {item["ingredient__name"].capitalize()} --'
             f' {item["total_amount"]} {item["ingredient__measurement_unit"]}'
-            for index, item in enumerate(ingredient_quantities)
+            for index, item in enumerate(ingredient_quantities, start=1)
         ]
     )
     recipe_header = 'Рецепты:'
     recipes_list = '\n'.join(
-        [f'{index + 1}. {recipe}' for index, recipe in enumerate(recipes)]
+        [f'{index}. {recipe}' for index, recipe in enumerate(recipes, start=1)]
     )
 
     return '\n'.join([header, product_header, products,
