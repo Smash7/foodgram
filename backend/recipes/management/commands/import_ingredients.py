7f002aa1923c72865ce79c589bdbd8e002d7100b
Eugene Martynov <martynov005@mail.ru>
2024-08-05 19:00:11 +0200

    refactor import_ingredients.py

--- backend/recipes/management/commands/import_ingredients.py
+++ backend/recipes/management/commands/import_ingredients.py
@@ -1,16 +1,27 @@
+import json
 import os
 
 from django.conf import settings
-from django.core.management import call_command
 from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
 
 
 class Command(BaseCommand):
     help = 'Импорт ингредиентов из data/ingredients.json'
 
     def handle(self, *args, **kwargs):
-        file_path = os.path.join(
-            settings.BASE_DIR.parent,
-            'data/ingredients.json'
-        )
-        call_command('loaddata', file_path)
+        file_path = os.path.join(settings.BASE_DIR.parent,
+                                 'data/ingredients.json')
+
+        if not os.path.exists(file_path):
+            self.stdout.write(self.style.ERROR(f'Файл {file_path} не найден'))
+            return
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for item in data:
+                Ingredient.objects.create(
+                    name=item['name'],
+                    measurement_unit=item['measurement_unit']
+                )
